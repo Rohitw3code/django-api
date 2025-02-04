be2c44a1daeee4c5fa25aac5ca4f4a527cd4ddfb
Beluga <rohitcode005@gmail.com>
2025-02-04 15:19:26 +0530

    added forign key in employee

--- api/models.py
+++ api/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Company(models.Model):
-    compnay_id = models.AutoField(primary_key=True)
+    company_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     about = models.TextField()
 
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    position = models.CharField(max_length=100)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
--- api/urls.py
+++ api/urls.py
@@ -10,18 +10,20 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URLconf 
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path ,include
-from api.views import CompnayViewSet
+from api.views import CompnayViewSet,EmployeeViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'companies',CompnayViewSet)
+router.register(r'employee',EmployeeViewSet)
 
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
 ]
